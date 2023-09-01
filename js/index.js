05754c62a78bd30f3929e5fea9cf3d5d259029ef
Aslanfix <aslanbekazimov777@gmail.com>
2023-09-01 20:19:02 +0300

    fily

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let dark__mode = document.querySelector('#darkmode')
+let elBody = document.querySelector('#body')
+
+
+
+dark__mode.addEventListener('click', function () {
+    elBody.classList.toggle('dark')
+    
+})
